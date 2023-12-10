5f8233580c2f8c63dd9289b848be4edebec28c33
David R. MacIver <david@drmaciver.com>
2023-12-10 11:37:56 +0000

    Make clang delta calls asynchronous

--- src/shrinkray/passes/clangdelta.py
+++ src/shrinkray/passes/clangdelta.py
@@ -1,45 +1,113 @@
-from lib2to3.fixes.fix_next import is_assign_target
 import os
 import subprocess
-import sys
 from tempfile import NamedTemporaryFile
+import trio
 
 from shrinkray.problem import ReductionProblem
 from shrinkray.reducer import ReductionPass
 from shrinkray.work import NotFound
 
 
+TRANSFORMATIONS = [
+    "aggregate-to-scalar",
+    "binop-simplification",
+    "callexpr-to-value",
+    "class-template-to-class",
+    "combine-global-var",
+    "combine-local-var",
+    "copy-propagation",
+    "empty-struct-to-int",
+    "expression-detector",
+    "instantiate-template-param",
+    "instantiate-template-type-param-to-int",
+    "lift-assignment-expr",
+    "local-to-global",
+    "move-function-body",
+    "move-global-var",
+    "param-to-global",
+    "param-to-local",
+    "reduce-array-dim",
+    "reduce-array-size",
+    "reduce-class-template-param",
+    "reduce-pointer-level",
+    "reduce-pointer-pairs",
+    "remove-addr-taken",
+    "remove-array",
+    "remove-base-class",
+    "remove-ctor-initializer",
+    "remove-enum-member-value",
+    "remove-namespace",
+    "remove-nested-function",
+    "remove-pointer",
+    "remove-trivial-base-template",
+    "remove-unresolved-base",
+    "remove-unused-enum-member",
+    "remove-unused-field",
+    "remove-unused-function",
+    "remove-unused-outer-class",
+    "remove-unused-var",
+    "rename-class",
+    "rename-cxx-method",
+    "rename-fun",
+    "rename-param",
+    "rename-var",
+    "replace-array-access-with-index",
+    "replace-array-index-var",
+    "replace-callexpr",
+    "replace-class-with-base-template-spec",
+    "replace-dependent-name",
+    "replace-dependent-typedef",
+    "replace-derived-class",
+    "replace-function-def-with-decl",
+    "replace-one-level-typedef-type",
+    "replace-simple-typedef",
+    "replace-undefined-function",
+    "return-void",
+    "simple-inliner",
+    "simplify-callexpr",
+    "simplify-comma-expr",
+    "simplify-dependent-typedef",
+    "simplify-if",
+    "simplify-nested-class",
+    "simplify-recursive-template-instantiation",
+    "simplify-struct",
+    "simplify-struct-union-decl",
+    "template-arg-to-int",
+    "template-non-type-arg-to-int",
+    "unify-function-decl",
+    "union-to-struct",
+    "vector-to-array",
+]
+
+
 class ClangDelta:
     def __init__(self, path):
         self.path_to_exec = path
 
-        self.transformations = [
-            line.strip()
-            for line in subprocess.check_output(
-                [path, "--transformations"], universal_newlines=True
-            ).splitlines()
-            if line.strip()
-        ]
+        self.transformations = TRANSFORMATIONS
 
     def __validate_transformation(self, transformation):
         if transformation not in self.transformations:
             raise ValueError(f"Invalid transformation {transformation}")
 
-    def query_instances(self, transformation: str, data: bytes) -> int:
+    async def query_instances(self, transformation: str, data: bytes) -> int:
         self.__validate_transformation(transformation)
         with NamedTemporaryFile(suffix=".cpp", delete_on_close=False) as tmp:
             tmp.write(data)
             tmp.close()
 
             try:
-                results = subprocess.check_output(
-                    [
-                        self.path_to_exec,
-                        f"--query-instances={transformation}",
-                        tmp.name,
-                    ],
-                    stderr=subprocess.PIPE,
-                )
+                results = (
+                    await trio.run_process(
+                        [
+                            self.path_to_exec,
+                            f"--query-instances={transformation}",
+                            tmp.name,
+                        ],
+                        capture_stdout=True,
+                        capture_stderr=True,
+                    )
+                ).stdout
             except subprocess.SubprocessError as e:
                 msg = (e.stdout + e.stderr).strip()
                 if msg == b"Error: Unsupported file type!":
@@ -55,7 +123,7 @@ class ClangDelta:
             assert results.startswith(prefix)
             return int(results[len(prefix) :].strip().decode("ascii"))
 
-    def apply_transformation(
+    async def apply_transformation(
         self, transformation: str, counter: int, data: bytes
     ) -> bytes:
         self.__validate_transformation(transformation)
@@ -64,15 +132,18 @@ class ClangDelta:
             tmp.close()
 
             try:
-                return subprocess.check_output(
-                    [
-                        self.path_to_exec,
-                        f"--transformation={transformation}",
-                        f"--counter={int(counter)}",
-                        tmp.name,
-                    ],
-                    stderr=subprocess.PIPE,
-                )
+                return (
+                    await trio.run_process(
+                        [
+                            self.path_to_exec,
+                            f"--transformation={transformation}",
+                            f"--counter={int(counter)}",
+                            tmp.name,
+                        ],
+                        capture_stdout=True,
+                        capture_stderr=True,
+                    )
+                ).stdout
             except subprocess.SubprocessError as e:
                 if e.stdout.strip() == b"Error: Unsupported file type!":
                     raise ValueError("Not a C or C++ test case")
@@ -94,15 +165,22 @@ class ClangDeltaError(Exception):
 def clang_delta_pump(clang_delta: ClangDelta, transformation: str) -> bytes:
     async def apply(problem: ReductionProblem[bytes]):
         target = problem.current_test_case
+        assert target is not None
         try:
-            n = clang_delta.query_instances(transformation, target)
+            n = await clang_delta.query_instances(transformation, target)
         except ValueError:
-            return
+            import traceback
+
+            traceback.print_exc()
+            return target
         i = 1
         while i <= n:
 
             async def can_apply(j):
-                attempt = clang_delta.apply_transformation(transformation, j, target)
+                attempt = await clang_delta.apply_transformation(
+                    transformation, j, target
+                )
+                assert attempt is not None
                 if attempt == target:
                     return False
                 return await problem.is_interesting(attempt)
@@ -118,6 +196,7 @@ def clang_delta_pump(clang_delta: ClangDelta, transformation: str) -> bytes:
                     return target
 
             target = clang_delta.apply_transformation(transformation, i, target)
+            assert target is not None
             n = clang_delta.query_instances(transformation, target)
         return target
 

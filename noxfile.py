a49f947b270135dddea32bd1ade6c69acc3317b3
David R. MacIver <david@drmaciver.com>
2023-12-23 23:13:51 +0000

    Update python version in nox

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ except ImportError:
 
 
 package = "shrinkray"
-python_versions = ["3.10"]
+python_versions = ["3.12"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",

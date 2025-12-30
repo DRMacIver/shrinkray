8cb43b916444105ac1c42179ad9d43ec2e2543a6
David R. MacIver <david@drmaciver.com>
2025-12-30 12:55:14 +0000

    Improve also interesting check to not require runnability

--- gallery/enterprise-hello/ishello.sh
+++ gallery/enterprise-hello/ishello.sh
@@ -2,8 +2,15 @@
 
 set -eux
 
-python hello.py > hello.log
+# Check if valid python
+python -c '
+import ast
 
+with open("hello.py") as f:
+    ast.parse(f.read())
+'
+
+# If it crashes libcst, that's also interesting
 uv run --with 'libcst==1.8.6' python -c '
 import libcst as cst
 
@@ -11,4 +18,8 @@ with open("hello.py") as f:
     cst.parse_module(f.read())
 ' || exit 101
 
+# Run it. If it fails, uninteresting.
+python hello.py > hello.log
+
+# If ranning it said hello, interesting
 grep "hello" hello.log

494fb4bae435466936a79a96ecdc34594543d210
David R. MacIver <david@drmaciver.com>
2025-12-30 11:59:49 +0000

    Add also interesting check to ishello.sh

--- gallery/enterprise-hello/ishello.sh
+++ gallery/enterprise-hello/ishello.sh
@@ -4,4 +4,11 @@ set -eux
 
 python hello.py > hello.log
 
+uv run --with 'libcst==1.8.6' python -c '
+import libcst as cst
+
+with open("hello.py") as f:
+    cst.parse_module(f.read())
+' || exit 101
+
 grep "hello" hello.log
